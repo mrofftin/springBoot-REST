b361271633342fb0606da9b5656cfcf2448ab69b
mrofftin <mrofftin@gmail.com>
2022-01-18 07:23:14 +0900

    REST API day02-03

--- src/main/java/co/mr/restapi/mybatis/CompanyMapper.java
+++ src/main/java/co/mr/restapi/mybatis/CompanyMapper.java
@@ -8,6 +8,8 @@ import java.util.List;
 @Mapper
 public interface CompanyMapper {
     @Insert("insert into company(company_name, company_address) values(#{company.name}, #{company.address})")
+    // 자동으로 생성되는 id값이 자바객체의 keyProperty id에 생성되도록 설정
+    @Options(useGeneratedKeys=true, keyProperty="id")
     int insert(@Param("company") Company company);
 
     @Select("select * from company")
